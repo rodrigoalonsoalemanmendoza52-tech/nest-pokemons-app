f8cf35775b76280941e74a32b006b1ccf4555999
Rodrigo Alemán <rodrigoalonsoalemanmendoza52@gmail.com>
2026-02-11 10:42:51 -0600

    fix bootstrap structure

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,24 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-app.setGlobalPrefix('api/v2');
+  app.setGlobalPrefix('api/v2');
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
-app.useGlobalPipes(  
-  new ValidationPipe({ 
-whitelist: true, 
-forbidNonWhitelisted: true, 
-transform: true,
-transformOptions: {
-enableImplicitConversion: true,
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+
+  console.log(`Application is running on: ${port}`);
 }
 
-  }) 
-);
-
-
-  await app.listen(process.env.PORT || 3001);}
-  console.log(`Application is running on: ${process.env.PORT || 3001}`);
 bootstrap();
+
